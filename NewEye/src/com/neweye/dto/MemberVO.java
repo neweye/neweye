55db523d0d735360e80574dc4b74e5f5e2a62b39
jiyoung <amado2u@naver.com>
2016-12-20 21:11:29 +0900

    commit

--- NewEye/src/com/neweye/dto/MemberVO.java
+++ NewEye/src/com/neweye/dto/MemberVO.java
@@ -1,30 +1,23 @@
 package com.neweye.dto;
 
+import java.sql.Timestamp;
+
 public class MemberVO {
 	
-	private String name;
-	private String userid;
+	private String id;
 	private String password;
+	private String name;
 	private String email;
-		
-	public MemberVO(){}
-	public MemberVO(String name, String userid, String password, String email) {
-		this.name = name;
-		this.userid = userid;
-		this.password = password;
-		this.email = email;
+	private String zip_num;
+	private String address;
+	private String phone;
+	private String useyn;
+	private Timestamp indate;
+	public String getId() {
+		return id;
 	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getUserid() {
-		return userid;
-	}
-	public void setUserid(String userid) {
-		this.userid = userid;
+	public void setId(String id) {
+		this.id = id;
 	}
 	public String getPassword() {
 		return password;
@@ -32,12 +25,50 @@ public class MemberVO {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	public String getZip_num() {
+		return zip_num;
+	}
+	public void setZip_num(String zip_num) {
+		this.zip_num = zip_num;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getUseyn() {
+		return useyn;
+	}
+	public void setUseyn(String useyn) {
+		this.useyn = useyn;
+	}
+	public Timestamp getIndate() {
+		return indate;
+	}
+	public void setIndate(Timestamp indate) {
+		this.indate = indate;
+	}
+		
+	
 	
 	
 }
