aed6a61c39df3a9fd9f9b6b8f9f8f6fe9b661ee9
jar <piyi@daum.net>
2016-12-22 15:25:26 +0900

    cccc

--- NewEye/src/com/neweye/action/FindIdAction.java
+++ NewEye/src/com/neweye/action/FindIdAction.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.neweye.action;
 
 import java.io.IOException;
@@ -37,43 +36,3 @@ public class FindIdAction implements Action{
 	}
 
 }
-=======
-package com.neweye.action;
-
-import java.io.IOException;
-import java.sql.SQLException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.neweye.dao.MemberDAO;
-import com.neweye.dao.iBatis.MemberDAO_iBatis;
-import com.neweye.dto.MemberVO;
-
-public class FindIdAction implements Action{
-
-	@Override
-	public String execute(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		String url = "/member/findIdForm.jsp";
-		
-		String name = request.getParameter("name");
-		String phone = request.getParameter("phone");
-		MemberDAO memberDAO=MemberDAO_iBatis.getInstance();
-		MemberVO member=null;
-		try {
-			member = (MemberVO)memberDAO.searchId(name, phone);
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		request.setAttribute("member", member);
-		return url;
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/neweye/neweye.git
