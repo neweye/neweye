3bb925bfa7de8d376a55bc86b48ffa71d688f9e2
jar <piyi@daum.net>
2017-01-02 12:45:25 +0900

    ddddd

--- NewEye/src/com/neweye/admin/action/AdminQnaResaveAction.java
+++ NewEye/src/com/neweye/admin/action/AdminQnaResaveAction.java
@@ -26,7 +26,7 @@ public class AdminQnaResaveAction implements Action {
 		System.out.println("1111");
 		String qseq = request.getParameter("qseq").trim();
 		System.out.println("22222");
-		String reply = request.getParameter("reply").trim(); //여기서 에러
+		String reply = request.getParameter("reply"); //여기서 에러
 		System.out.println("33333");
 		System.out.println("테스트:"+request.getParameter(reply));
 
