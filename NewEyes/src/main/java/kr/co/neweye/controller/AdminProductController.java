c7a1f4528651bbefda27cb6c6220fd649701f372
Hong Hong <hoihoi207@naver.com>
2017-01-06 12:23:23 +0900

    commit >> master kjh product update >> by jar

--- NewEyes/src/main/java/kr/co/neweye/controller/AdminProductController.java
+++ NewEyes/src/main/java/kr/co/neweye/controller/AdminProductController.java
@@ -180,7 +180,7 @@ public class AdminProductController {
               
        ProductVO productVO = new ProductVO();
        
-       //productVO.setPseq(multi.getParameter("pseq")); //--------------------------
+       //productVO.setPseq(request.getParameter("pseq")); //--------------------------
        productVO.setFirst_level(request.getParameter("first_level")); //--------------------------
        productVO.setSecond_level(request.getParameter("second_level")); //--------------------------
        productVO.setKind(request.getParameter("kind")); //--------------------------
@@ -281,97 +281,92 @@ public class AdminProductController {
        return url;
      }
    
-//    @RequestMapping("/adminProductUpdate")
-//      public String adminProductUpdate(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//         String url = "redirect:adminProductList";
-//
-//         HttpSession session = request.getSession();
-//
-//         int sizeLimit = 5 * 1024 * 1024;
-//         String savePath = "productimg";
-//         ServletContext context = session.getServletContext();
-//         String uploadFilePath = context.getRealPath(savePath);
-//         
-//         MultipartRequest multi = new MultipartRequest(request,
-//             uploadFilePath,
-//             sizeLimit,
-//             "UTF-8",
-//             new DefaultFileRenamePolicy()
-//         );
-//         
-//         ProductVO productVO = new ProductVO();
-//         productVO.setPseq(Integer.parseInt(multi.getParameter("pseq"))); //--------------------------
-//         productVO.setFirst_level(multi.getParameter("first_level")); //--------------------------
-//         productVO.setSecond_level(multi.getParameter("second_level")); //--------------------------
-//         productVO.setKind(multi.getParameter("kind")); //--------------------------
-//         productVO.setName(multi.getParameter("name")); //--------------------------
-//         String indate=multi.getParameter("indate")+" 10:20:30.0";
-//         productVO.setIndate(java.sql.Timestamp.valueOf(indate)); //--------------------------
-//         
-//         productVO.setRead_count(0);
-//         if(multi.getParameter("price")==null||multi.getParameter("price").equals("")) { productVO.setPrice(0);
-//         }else{ productVO.setPrice(Integer.parseInt(multi.getParameter("price"))); }
-//         if(multi.getParameter("weight")==null||multi.getParameter("weight").equals("")) { productVO.setWeight(0);
-//         }else{ productVO.setWeight(Integer.parseInt(multi.getParameter("weight"))); }
-//         if(multi.getParameter("quantity")==null||multi.getParameter("quantity").equals("")) { productVO.setQuantity(0);
-//         }else{ productVO.setQuantity(Integer.parseInt(multi.getParameter("quantity"))); }
-//         if(multi.getParameter("weight")==null||multi.getParameter("weight").equals("")) { productVO.setWeight(0);
-//         }else{ productVO.setWeight(Integer.parseInt(multi.getParameter("weight"))); }
-//         if(multi.getParameter("pixel")==null||multi.getParameter("pixel").equals("")) { productVO.setPixel(0);
-//         }else{ productVO.setPixel(Integer.parseInt(multi.getParameter("pixel"))); }
-//         if(multi.getParameter("speed")==null||multi.getParameter("speed").equals("")) { productVO.setSpeed(0);
-//         }else{ productVO.setSpeed(Integer.parseInt(multi.getParameter("speed"))); }
-//         if(multi.getParameter("seqpictures")==null||multi.getParameter("seqpictures").equals("")) { productVO.setSeqpictures(0);
-//         }else{ productVO.setSeqpictures(Integer.parseInt(multi.getParameter("seqpictures"))); }
-//         if(multi.getParameter("iso")==null||multi.getParameter("iso").equals("")) { productVO.setIso(0);
-//         }else{ productVO.setIso(Integer.parseInt(multi.getParameter("iso"))); }
-//         if(multi.getParameter("movframe")==null||multi.getParameter("movframe").equals("")) { productVO.setMovframe(0);
-//         }else{ productVO.setMovframe(Integer.parseInt(multi.getParameter("movframe"))); }
-//         if(multi.getParameter("filter")==null||multi.getParameter("filter").equals("")) { productVO.setFilter(0);
-//         }else{ productVO.setFilter(Integer.parseInt(multi.getParameter("filter"))); }
-//         
-//         if(multi.getParameter("ratio")==null||multi.getParameter("ratio").equals("")) { productVO.setRatio(0);
-//         }else{ productVO.setRatio(Float.parseFloat(multi.getParameter("ratio"))); }
-//         if(multi.getParameter("screen")==null||multi.getParameter("screen").equals("")) { productVO.setScreen(0);
-//         }else{ productVO.setScreen(Float.parseFloat(multi.getParameter("screen"))); }
-//         if(multi.getParameter("minfocus")==null||multi.getParameter("minfocus").equals("")) { productVO.setMinfocus(0);
-//         }else{ productVO.setMinfocus(Float.parseFloat(multi.getParameter("minfocus"))); }
-//         if(multi.getParameter("maxfocus")==null||multi.getParameter("maxfocus").equals("")) { productVO.setMaxfocus(0);
-//         }else{ productVO.setMaxfocus(Float.parseFloat(multi.getParameter("maxfocus"))); }
-//         if(multi.getParameter("minaperture")==null||multi.getParameter("minaperture").equals("")) { productVO.setMinaperture(0);
-//         }else{ productVO.setMinaperture(Float.parseFloat(multi.getParameter("minaperture"))); }
-//         if(multi.getParameter("maxaperture")==null||multi.getParameter("maxaperture").equals("")) { productVO.setMaxaperture(0);
-//         }else{ productVO.setMaxaperture(Float.parseFloat(multi.getParameter("maxaperture"))); }
-//         if(multi.getParameter("distance")==null||multi.getParameter("distance").equals("")) { productVO.setDistance(0);
-//         }else{ productVO.setDistance(Float.parseFloat(multi.getParameter("distance"))); }
-//         
-//         productVO.setUseyn(multi.getParameter("useyn"));
-//         productVO.setContent(multi.getParameter("content"));
-//         productVO.setSizee(multi.getParameter("sizee"));
-//         productVO.setFormat(multi.getParameter("format"));
-//         productVO.setFunctions(multi.getParameter("functions"));
-//         productVO.setZoomyn(multi.getParameter("zoomyn"));
-//         productVO.setTypes(multi.getParameter("types"));
-//         if(multi.getFilesystemName("img_list")==null||multi.getFilesystemName("img_list").equals("")){
-//            productVO.setImg_list(multi.getParameter("img_list_hidden"));
-//         }else{
-//            productVO.setImg_list(multi.getFilesystemName("img_list"));
-//         }
-//         if(multi.getFilesystemName("img_detail")==null||multi.getFilesystemName("img_detail").equals("")){
-//            productVO.setImg_list(multi.getParameter("img_detail_hidden"));
-//         }else{
-//            productVO.setImg_detail(multi.getFilesystemName("img_detail"));
-//         }
-//         ProductDAO productDAO = ProductDAO_iBatis.getInstance();
-//         try {
-//            productDAO.updateProduct(productVO);
-//         } catch (SQLException e) {
-//            e.printStackTrace();
-//         }
-//
-//         return url;
-//      }
+    @RequestMapping("/adminProductUpdate")
+      public String adminProductUpdate(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         String url = "redirect:adminProductList";
+
+         HttpSession session = request.getSession();
+
+         int sizeLimit = 5 * 1024 * 1024;
+         String savePath = "productimg";
+         ServletContext context = session.getServletContext();
+         String uploadFilePath = context.getRealPath(savePath);
+         
+         
+         
+         ProductVO productVO = new ProductVO();
+         productVO.setPseq(Integer.parseInt(request.getParameter("pseq"))); //--------------------------
+         productVO.setFirst_level(request.getParameter("first_level")); //--------------------------
+         productVO.setSecond_level(request.getParameter("second_level")); //--------------------------
+         productVO.setKind(request.getParameter("kind")); //--------------------------
+         productVO.setName(request.getParameter("name")); //--------------------------
+         String indate=request.getParameter("indate")+" 10:20:30.0";
+         productVO.setIndate(java.sql.Timestamp.valueOf(indate)); //--------------------------
+         
+         productVO.setRead_count(0);
+         if(request.getParameter("price")==null||request.getParameter("price").equals("")) { productVO.setPrice(0);
+         }else{ productVO.setPrice(Integer.parseInt(request.getParameter("price"))); }
+         if(request.getParameter("weight")==null||request.getParameter("weight").equals("")) { productVO.setWeight(0);
+         }else{ productVO.setWeight(Integer.parseInt(request.getParameter("weight"))); }
+         if(request.getParameter("quantity")==null||request.getParameter("quantity").equals("")) { productVO.setQuantity(0);
+         }else{ productVO.setQuantity(Integer.parseInt(request.getParameter("quantity"))); }
+         if(request.getParameter("weight")==null||request.getParameter("weight").equals("")) { productVO.setWeight(0);
+         }else{ productVO.setWeight(Integer.parseInt(request.getParameter("weight"))); }
+         if(request.getParameter("pixel")==null||request.getParameter("pixel").equals("")) { productVO.setPixel(0);
+         }else{ productVO.setPixel(Integer.parseInt(request.getParameter("pixel"))); }
+         if(request.getParameter("speed")==null||request.getParameter("speed").equals("")) { productVO.setSpeed(0);
+         }else{ productVO.setSpeed(Integer.parseInt(request.getParameter("speed"))); }
+         if(request.getParameter("seqpictures")==null||request.getParameter("seqpictures").equals("")) { productVO.setSeqpictures(0);
+         }else{ productVO.setSeqpictures(Integer.parseInt(request.getParameter("seqpictures"))); }
+         if(request.getParameter("iso")==null||request.getParameter("iso").equals("")) { productVO.setIso(0);
+         }else{ productVO.setIso(Integer.parseInt(request.getParameter("iso"))); }
+         if(request.getParameter("movframe")==null||request.getParameter("movframe").equals("")) { productVO.setMovframe(0);
+         }else{ productVO.setMovframe(Integer.parseInt(request.getParameter("movframe"))); }
+         if(request.getParameter("filter")==null||request.getParameter("filter").equals("")) { productVO.setFilter(0);
+         }else{ productVO.setFilter(Integer.parseInt(request.getParameter("filter"))); }
+         
+         if(request.getParameter("ratio")==null||request.getParameter("ratio").equals("")) { productVO.setRatio(0);
+         }else{ productVO.setRatio(Float.parseFloat(request.getParameter("ratio"))); }
+         if(request.getParameter("screen")==null||request.getParameter("screen").equals("")) { productVO.setScreen(0);
+         }else{ productVO.setScreen(Float.parseFloat(request.getParameter("screen"))); }
+         if(request.getParameter("minfocus")==null||request.getParameter("minfocus").equals("")) { productVO.setMinfocus(0);
+         }else{ productVO.setMinfocus(Float.parseFloat(request.getParameter("minfocus"))); }
+         if(request.getParameter("maxfocus")==null||request.getParameter("maxfocus").equals("")) { productVO.setMaxfocus(0);
+         }else{ productVO.setMaxfocus(Float.parseFloat(request.getParameter("maxfocus"))); }
+         if(request.getParameter("minaperture")==null||request.getParameter("minaperture").equals("")) { productVO.setMinaperture(0);
+         }else{ productVO.setMinaperture(Float.parseFloat(request.getParameter("minaperture"))); }
+         if(request.getParameter("maxaperture")==null||request.getParameter("maxaperture").equals("")) { productVO.setMaxaperture(0);
+         }else{ productVO.setMaxaperture(Float.parseFloat(request.getParameter("maxaperture"))); }
+         if(request.getParameter("distance")==null||request.getParameter("distance").equals("")) { productVO.setDistance(0);
+         }else{ productVO.setDistance(Float.parseFloat(request.getParameter("distance"))); }
+         
+         productVO.setUseyn(request.getParameter("useyn"));
+         productVO.setContent(request.getParameter("content"));
+         productVO.setSizee(request.getParameter("sizee"));
+         productVO.setFormat(request.getParameter("format"));
+         productVO.setFunctions(request.getParameter("functions"));
+         productVO.setZoomyn(request.getParameter("zoomyn"));
+         productVO.setTypes(request.getParameter("types"));
+         if(request.getParameter("img_list")==null||request.getParameter("img_list").equals("")){
+            productVO.setImg_list(request.getParameter("img_list_hidden"));
+         }else{
+            productVO.setImg_list(request.getParameter("img_list"));
+         }
+         if(request.getParameter("img_detail")==null||request.getParameter("img_detail").equals("")){
+            productVO.setImg_list(request.getParameter("img_detail_hidden"));
+         }else{
+            productVO.setImg_detail(request.getParameter("img_detail"));
+         }
+         ProductDAO productDAO = ProductDAO_iBatis.getInstance();
+         try {
+            productDAO.updateProduct(productVO);
+         } catch (SQLException e) {
+            e.printStackTrace();
+         }
+
+         return url;
+      }
    
     
     
